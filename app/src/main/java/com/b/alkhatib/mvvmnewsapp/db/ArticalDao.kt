f01da035291322a2bcb8a648de6913f9aaf34b84
bilal0499 <87735826+bilal0499@users.noreply.github.com>
2023-01-27 23:06:06 +0200

    DAO Article Setup Room : - create the Artical Dao Interface that include:   insert,update,delete functions

---
+++ app/src/main/java/com/b/alkhatib/mvvmnewsapp/db/ArticalDao.kt
@@ -0,0 +1,21 @@
+package com.b.alkhatib.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.b.alkhatib.mvvmnewsapp.Article
+
+@Dao
+interface ArticalDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) //onConflict: this strategy determine what happens if that article that we want to insert in that database already exists in our database and in that case we simply want to replace
+    suspend fun upsert(article: Article):Long       // which stands for update or insert so it will insert a new article and if it is already in the database it will update it inserted and replace it. Long: which is the id that was insered
+
+    // this function should return all available articles in our database // why it is not a suspend fun? -> because it will return a LiveData object and that doesnt work with suspend fun
+    @Query("SELECT * from article")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+
+}
